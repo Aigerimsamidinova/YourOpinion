f8b60aa68b8824e2520628610dc531a1f6919bcf
Astik <serikovastik@mail.com>
2019-05-12 20:22:57 +0600

    Added full User Services

--- src/main/java/com/example/projectXX/controller/UserController.java
+++ src/main/java/com/example/projectXX/controller/UserController.java
@@ -1,4 +1,79 @@
 package com.example.projectXX.controller;
 
+import com.example.projectXX.model.User;
+import com.example.projectXX.model.User;
+import com.example.projectXX.service.CrudService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private CrudService<User> userCrudService;
+
+    @GetMapping("/getAll")
+    public List<User> getUsers() {
+        return this.userCrudService.getAll();
+    }
+
+    @GetMapping("/getById/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = this.userCrudService.findById(id);
+        try {
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/post")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<User> saveUser(@RequestBody User c) {
+        User user = this.userCrudService.save(c);
+        try {
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<User> updateUser(@RequestBody User c) {
+        User user = this.userCrudService.update(c);
+        try {
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/deleteById/{id}")
+    public ResponseEntity<String> deleteUserById(@PathVariable Long id) {
+        try {
+            this.userCrudService.deleteById(id);
+            return new ResponseEntity<>("Successfully deleted", HttpStatus.OK);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            return new ResponseEntity<>("Not deleted", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/deleteAll")
+    public ResponseEntity<String> deleteAllUsers() {
+        try {
+            this.userCrudService.deleteAll();
+            return new ResponseEntity<>("All users deleted", HttpStatus.OK);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            return new ResponseEntity<>("Not deleted", HttpStatus.BAD_REQUEST);
+        }
+    }
 }
+
--- src/main/java/com/example/projectXX/model/User.java
+++ src/main/java/com/example/projectXX/model/User.java
@@ -1,4 +1,37 @@
 package com.example.projectXX.model;
 
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+
+@Entity
+@Table(name="users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Email
+    private String email;
+
+    public User(@Email String email) {
+        this.email = email;
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
